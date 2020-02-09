7b82034c2eb883830a52bdc9e6eb3455c5545432
ngoccuong <ngoccuonggl249@gmail.com>
2020-02-09 16:46:33 +0700

    Add switch Dark/Light mode

--- src/components/TodoInput/components/TodoInput.view.js
+++ src/components/TodoInput/components/TodoInput.view.js
@@ -1,18 +1,28 @@
 import React from 'react'
+import styled from '@emotion/styled';
 
 import style from '../assets/TodoInput.module.scss'
 
+const Wrapper = styled("div")`
+  input {
+    color: ${props => props.theme.text};
+    background: ${props => props.theme.inputBackground};
+  }
+`;
+
 const TodoInputView = ({ taskContent, handleChangeInput, handleKeyPressed }) => {
   return (
-    <div className={style.todoInput__container}>
-      <input
-        className={style.todoInput__input}
-        placeholder='Enter todo name here'
-        value={taskContent}
-        onChange={handleChangeInput}
-        onKeyPress={handleKeyPressed}
-      />
-    </div>
+    <Wrapper>
+      <div className={style.todoInput__container}>
+        <input
+          className={style.todoInput__input}
+          placeholder='Enter todo name here'
+          value={taskContent}
+          onChange={handleChangeInput}
+          onKeyPress={handleKeyPressed}
+        />
+      </div>
+    </Wrapper>
   )
 };
 
--- src/components/TodoList/components/TodoList.View.js
+++ src/components/TodoList/components/TodoList.View.js
@@ -1,11 +1,18 @@
 import React, { Fragment } from 'react'
 import _ from 'lodash'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import styled from '@emotion/styled';
+
 import Button from '../../../vendor/Button'
 import style from '../assets/TodoList.module.scss'
 
-const TodoListView = ({displayTasks, removeTask, setTaskDone, toggleDisplayTasks}) => {
+const ItemWrapper = styled("div")`
+  border-radius: 6px;
+  color: ${props => props.theme.text};
+  background: ${props => props.theme.inputBackground};
+`;
 
+const TodoListView = ({displayTasks, removeTask, setTaskDone, toggleDisplayTasks}) => {
   return (
     <Fragment>
       <div className={style.wrapper}>
@@ -16,10 +23,12 @@ const TodoListView = ({displayTasks, removeTask, setTaskDone, toggleDisplayTasks
                 key={task.id}
                 timeout={250}
               >
-              <div className={`${style.item} ${style[task.status]}`}>
-                <div className={style.content} onClick={() => setTaskDone(task)}>{task.content}</div>
-                <Button className={style.close_button} onClick={() => removeTask(task)}>x</Button>
-              </div>
+                <ItemWrapper>
+                  <div className={`${style.item} ${style[task.status]}`}>
+                    <div className={style.content} onClick={() => setTaskDone(task)}>{task.content}</div>
+                    <Button className={style.close_button} onClick={() => removeTask(task)}>x</Button>
+                  </div>
+                </ItemWrapper>
               </CSSTransition>
             ))
           }
